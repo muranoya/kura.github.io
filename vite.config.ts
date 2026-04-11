741ce287c7da47d23912ea023943c963c8b764ae
Daisuke MURAOKA <daisuke.muraoka.jp@gmail.com>
2026-04-12 00:47:55 +0900

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [tailwindcss()],
   root: "src",
+  base: "/kura.github.io/",
+  publicDir: "../public",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
